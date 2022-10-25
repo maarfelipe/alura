9e8945285ee3a833fbc94e9940d3398408f48cac
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-10-25 11:36:38 -0300

    change white -> for

--- formacaoPython_poo/01.adivinhacao.py
+++ formacaoPython_poo/01.adivinhacao.py
@@ -3,11 +3,10 @@ print('*********************************\n'
       '*********************************')
 
 numero_secreto = 42
-rodada = 1
 total_de_tentativas = 3
 
-while rodada <= total_de_tentativas:
-    print(f'Tentativa {rodada} de `{total_de_tentativas}')
+for rodada in range(1, total_de_tentativas + 1):
+    print(f'Tentativa {rodada} de {total_de_tentativas}')
     chute = int(input('Digite o seu número: '))
 
     acertou = chute == numero_secreto
@@ -21,7 +20,5 @@ while rodada <= total_de_tentativas:
             print('Errou! Seu chute foi maior que o número secreto.')
         elif menor:
             print('Errou! Seu chute foi mnor que o número secreto.')
-
-    rodada += 1
 
 print('Fim do jogo!')
