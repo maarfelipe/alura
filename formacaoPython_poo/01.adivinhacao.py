b6958a89a9944de5ea853dd59e1c546992292acc
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-10-25 13:07:00 -0300

    continue & break

--- formacaoPython_poo/01.adivinhacao.py
+++ formacaoPython_poo/01.adivinhacao.py
@@ -7,7 +7,11 @@ total_de_tentativas = 3
 
 for rodada in range(1, total_de_tentativas + 1):
     print(f'Tentativa {rodada} de {total_de_tentativas}')
-    chute = int(input('Digite o seu número: '))
+    chute = int(input('Digite um número entre 1 e 100: '))
+
+    if chute < 1 or chute > 100:
+        print('Você deve digitar um número entre 1 e 100!')
+        continue
 
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
@@ -15,10 +19,11 @@ for rodada in range(1, total_de_tentativas + 1):
 
     if acertou:
         print('Acertou!')
+        break
     else:
         if maior:
             print('Errou! Seu chute foi maior que o número secreto.')
         elif menor:
-            print('Errou! Seu chute foi mnor que o número secreto.')
+            print('Errou! Seu chute foi menor que o número secreto.')
 
 print('Fim do jogo!')
