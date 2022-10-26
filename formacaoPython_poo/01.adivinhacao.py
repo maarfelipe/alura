d3b44a0e404c1e725d3fa358eac089456fc916a2
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-10-26 09:08:49 -0300

    add dificuldade

--- formacaoPython_poo/01.adivinhacao.py
+++ formacaoPython_poo/01.adivinhacao.py
@@ -7,6 +7,18 @@ print('*********************************\n'
 numero_secreto = random.randrange(1, 101)
 total_de_tentativas = 3
 
+print('[1] Fácil\n'
+      '[2] Médio\n'
+      '[3] Difícil')
+nivel = input(int('Digite o nível de dificuldade: '))
+
+if nivel == 1:
+    total_de_tentativas = 20
+elif nivel == 2:
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+
 for rodada in range(1, total_de_tentativas + 1):
     print(f'Tentativa {rodada} de {total_de_tentativas}')
     chute = int(input('Digite um número entre 1 e 100: '))
