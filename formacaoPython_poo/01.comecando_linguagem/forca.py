1d6cea15eb082cef669145d34699c52056f92625
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-10-27 08:39:30 -0300

    add game loop

--- formacaoPython_poo/01.comecando_linguagem/forca.py
+++ formacaoPython_poo/01.comecando_linguagem/forca.py
@@ -1,10 +1,16 @@
 
 def jogar():
-      print('*********************************\n'
-            f'{"Bem vindo ao jogo de Forca!":^34}\n'
-            '*********************************')
+    print('*********************************\n'
+        f'{"Bem vindo ao jogo de Forca!":^34}\n'
+        '*********************************')
 
-      print('Fim do jogo!')
+    palavra_secreta = 'banana'
+    acertou = enforcou = False
+
+    while not acertou and not enforcou:
+        print('Jogando ...')
+
+    print('Fim do jogo!')
 
 if (__name__ == '__main__'):
-    jogar()
+jogar()
