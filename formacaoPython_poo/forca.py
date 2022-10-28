1992cf4b159c9d21684f9d7c5ce6e387b3f6c9a5
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-10-28 11:15:54 -0300

    List Comprehension

--- formacaoPython_poo/forca.py
+++ formacaoPython_poo/forca.py
@@ -4,7 +4,8 @@ def jogar():
           '*********************************')
 
     palavra_secreta = 'banana'.upper()
-    letras_acertadas = ['_', '_', '_', '_', '_', '_']
+    letras_acertadas = ['_' for letra in palavra_secreta]
+
     acertou = enforcou = False
     erros = 0
 
