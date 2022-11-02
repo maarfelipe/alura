19b908aa0616f41586f39fcb66fe4697dbb4914b
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-11-02 09:15:12 -0300

    iniciando OO

---
+++ formacaoPython_poo/02.OO/teste.py
@@ -0,0 +1,6 @@
+def criaConta(numero, titular, saldo, limite):
+    conta = {'numero': numero,
+             'titular': titular,
+             'saldo': saldo,
+             'limite': limite}
+    return conta
