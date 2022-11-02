854a8931c79583b9fa9bf539819135a167283a1e
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-11-02 12:11:16 -0300

    add docstrings nas functions

--- formacaoPython_poo/02.OO/conta.py
+++ formacaoPython_poo/02.OO/conta.py
@@ -2,6 +2,13 @@ class Conta:
 
     # atributos
     def __init__(self, numero, titular, saldo, limite=1000.0):
+        '''
+        Criando os atributos PRIVADOS (__) para a classe Conta
+        :param numero: numero da conta
+        :param titular: nome do titular
+        :param saldo: saldo inicial
+        :param limite: limite recebe um valor padrão não sendo necessário especificar durante a criação do objeto
+        '''
         print(f'Construindo objeto ... {self}')
         self.__numero = numero
         self.__titular = titular
@@ -10,14 +17,34 @@ class Conta:
 
     # metodos
     def extrato(self):
+        '''
+        Método demonstrativo para acessar um atributo da conta
+        :return: frase com saldo e nome do titular da conta
+        '''
         print(f'Saldo R$ {self.__saldo:.2f} do titular {self.__titular}')
 
     def depositar(self, valor):
+        '''
+        Recebe um valor e o acrecenta no saldo da conta
+        :param valor: recebe o valor que será acrescentado
+        :return: atualiza o valor do saldo
+        '''
         self.__saldo += valor
 
     def sacar(self, valor):
+        '''
+        Recebe um valor e o diminui do saldo da conta
+        :param valor: recebe o valor que será subtraído
+        :return: atualiza o valor do saldo
+        '''
         self.__saldo -= valor
 
     def transferir(self, valor, contaDestino):
+        '''
+        Acessa duas contas para fazer uma transferência de valores entre elas
+        :param valor: recebe qual o valor será transferido
+        :param contaDestino: parâmetro que define qual conta vai receber a trasnferência
+        :return: altera o saldo nas duas contas
+        '''
         self.sacar(valor)
         contaDestino.depositar(valor)
