18ec4036d7646deb76e477fc16ef460dd0fc2662
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-11-02 10:02:08 -0300

    add metodos

--- formacaoPython_poo/02.OO/conta.py
+++ formacaoPython_poo/02.OO/conta.py
@@ -1,8 +1,17 @@
 class Conta:
 
-    def __init__(self, numero, titular, saldo, limite):
+    def __init__(self, numero, titular, saldo, limite=1000.0):
         print(f'Construindo objeto ... {self}')
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def extrato(self):
+        print(f'Saldo R$ {self.saldo:.2f} do titular {self.titular}')
+
+    def depositar(self, valor):
+        self.saldo += valor
+
+    def sacar(self, valor):
+        self.saldo -= valor
