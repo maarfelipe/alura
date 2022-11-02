c5edcc47bd0fb5a55462883f463220a2449d2325
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-11-02 09:46:27 -0300

    definindo atributos para classe

--- formacaoPython_poo/02.OO/conta.py
+++ formacaoPython_poo/02.OO/conta.py
@@ -1,3 +1,8 @@
 class Conta:
-    
-    pass
+
+    def __init__(self, numero, titular, saldo, limite):
+        print(f'Construindo objeto ... {self}')
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
