c057b8b4625004b45957c1e896b2f34ca7449de9
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-11-05 11:07:28 -0300

    iniciando o módulo 3
    classes filme e serie

---
+++ formacaoPython_poo/03.OO+/model.py
@@ -0,0 +1,16 @@
+class Filme:
+
+    def __init__(self, nome, ano, duracao):
+        self.nome = nome
+        self.ano = ano
+        self.duracao = duracao
+
+
+class Serie:
+
+    def __init__(self, nome, ano, temporadas):
+        self.nome = nome
+        self.ano = ano
+        self.duracao = temporadas
+
+
