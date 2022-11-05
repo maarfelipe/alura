40317fb4597a407c849666d4426de62694e9c756
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-11-05 14:16:48 -0300

    add getters e setters

--- formacaoPython_poo/03.OO+/model.py
+++ formacaoPython_poo/03.OO+/model.py
@@ -1,16 +1,48 @@
 class Filme:
-
+    # atributos e construtor
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.__nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.__likes = 0
+
+    # getters & setters
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novoNome):
+        self.__nome = novoNome.title()
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def darLike(self):
+        self.__likes += 1
 
 
 class Serie:
-
+    # atributos e construtor
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+        self.__nome = nome.title()
         self.ano = ano
         self.duracao = temporadas
+        self.__likes = 0
 
+    # getters & setters
+    @property
+    def nome(self):
+        return self.__nome
 
+    @nome.setter
+    def nome(self, novoNome):
+        self.__nome = novoNome.title()
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def darLike(self):
+        self.__likes += 1
