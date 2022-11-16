5f1e5d424009f44801d1863ef98d87d94f68d8c1
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-11-16 20:18:44 -0300

    add multiplos parâmetros

--- formacaoPython_poo/04.Strings/01.fatiamentoStrings.py
+++ formacaoPython_poo/04.Strings/01.fatiamentoStrings.py
@@ -1,9 +1,26 @@
-url = "bytebank.com/cambio?moedaOrigem=real"
-print(url)
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
 
-url_base = url[0:19]
-print(url_base)  # Vai imprimir “bytebank.com/cambio”
+# varre a URL até o valor em .find
+indiceInterrogacao = url.find('?')
+# guarda na variável urlBase o valor do início da string até o índice informado
+urlBase = url[:indiceInterrogacao]
+# parte do índice informado até o final da string e guarda o conteúdo na variável urlParametros
+urlParametros = url[indiceInterrogacao+1:]
 
-url_parametros = url[20:36]
-print(url_parametros)
+# a string informada é guardada na variável parametroBusca
+parametroBusca = 'quantidade'
+# busca o parâmetro informado acima dentro da parte de parâmetros da URL
+indiceParametro = urlParametros.find(parametroBusca)
+# ao encontrar o parâmetro informado, soma seu length + 1 para guardar o valor do parâmetro da URL
+indiceValor = indiceParametro + len(parametroBusca) + 1
 
+# na parte de parâmetros da URL, .find busca a partir do indiceValor um &
+eComercial = urlParametros.find('&', indiceValor)
+# se não encontrar o &
+if eComercial == -1:
+    # a variável recebe o valor do parâmetro até o final da string (quer dizer que é está no final da string)
+    valor = urlParametros[indiceValor:]
+else:
+    # se encontrar o & a variável salva o valor do parâmetro até o & (o valor da busca está no meio da string)
+    valor = urlParametros[indiceValor:eComercial]
+print(valor)
