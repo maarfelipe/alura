5d373e084005c9bb0af2dc4c9a31d9779a15bfe6
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-11-16 18:21:48 -0300

    iniciando fatiamento de strings

---
+++ formacaoPython_poo/04.Strings/01.fatiamentoStrings.py
@@ -0,0 +1,9 @@
+url = "bytebank.com/cambio?moedaOrigem=real"
+print(url)
+
+url_base = url[0:19]
+print(url_base)  # Vai imprimir “bytebank.com/cambio”
+
+url_parametros = url[20:36]
+print(url_parametros)
+
