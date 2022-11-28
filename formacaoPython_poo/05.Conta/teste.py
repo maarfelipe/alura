98fda25c16800c050fd137738ed6102628971857
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-11-28 19:00:38 -0300

    inciando módulo collections

---
+++ formacaoPython_poo/05.Conta/teste.py
@@ -0,0 +1,10 @@
+from ContaCorrente import *
+
+contaGui = ContaCorrente(15)
+contaGui.depositar(500)
+print(contaGui)
+
+contaDani = ContaCorrente(47685)
+contaDani.depositar(1000)
+print(contaDani)
+
