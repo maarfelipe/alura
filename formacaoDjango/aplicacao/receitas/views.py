42169d94bcad56b46b99f7406b87b7fc0bca5b7e
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-12-25 21:13:23 -0300

    iniciando postgres

--- formacaoDjango/aplicacao/receitas/views.py
+++ formacaoDjango/aplicacao/receitas/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+
+    receitas = {
+        1: 'Receita1',
+        2: 'Receita2',
+        3: 'Receita3'
+    }
+
+    dados = {
+        'nome_das_receitas': receitas
+    }
+
+    return render(request, 'index.html', dados)
 
 
 def receita(request):
