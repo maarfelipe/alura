4f519cec152a9ac25ace9e100f3883b9051483f0
maarfelipe <101366073+maarfelipe@users.noreply.github.com>
2022-12-29 14:38:30 -0300

    Delete formacaoDjango/aplicacao directory

--- formacaoDjango/aplicacao/receitas/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Receita
-
-
-class exibir_receita(admin.ModelAdmin):
-    list_display = ('nome_receita',)
-
-
-admin.site.register(Receita, exibir_receita)
