c326e1e39a4e76ad52e256156863d4c1abbfd8af
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-12-31 15:59:14 -0300

    url names (conectando todas as pages)

--- formacaoDjango/alura-receitas/receitas/urls.py
+++ formacaoDjango/alura-receitas/receitas/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from receitas.views import index
+from . import views
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', views.index, name='index'),
+    path('receita', views.receita, name='receita')
 ]
