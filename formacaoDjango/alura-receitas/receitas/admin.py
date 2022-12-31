138668236a776761e3a1183a0f2ada735d4920f1
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-12-31 20:08:02 -0300

    iniciando projeto 2.0
    add busca, filtros e paginação

--- formacaoDjango/alura-receitas/receitas/admin.py
+++ formacaoDjango/alura-receitas/receitas/admin.py
@@ -3,7 +3,11 @@ from .models import Receita
 
 
 class ExibirReceita(admin.ModelAdmin):
-    list_display = ('id', 'nome_receita')
+    list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo')
+    list_display_links = ('id', 'nome_receita')
+    search_fields = ('nome_receita',)
+    list_filter = ('categoria',)
+    list_per_page = 10
 
 
 admin.site.register(Receita, ExibirReceita)
