20e890c2c5ef0767a45e053f59e98641f55ccb79
Matheus Felipe <matheus.maarfe@hotmail.com>
2022-12-31 17:23:03 -0300

    add models & tables

--- formacaoDjango/alura-receitas/setup/settings.py
+++ formacaoDjango/alura-receitas/setup/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alura_receita',
+        'USER': 'postgres',
+        'PASSWORD': 'y59*m3Ap',
+        'HOST': 'localhost'
     }
 }
 
